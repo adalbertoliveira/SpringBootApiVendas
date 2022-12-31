64c20767eeae99fa416dc553981a59ebef49b00e
Adalberto Oliveira <oliver.adalberto@hotmail.com>
2022-12-30 21:42:22 -0300

    Branch com os exemplos de annotarions

--- src/main/java/org/adalbertoFerreira/VendasApplication.java
+++ src/main/java/org/adalbertoFerreira/VendasApplication.java
@@ -1,7 +1,5 @@
 package org.adalbertoFerreira;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
